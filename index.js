839d472782dd6e2f3b39e688d7c40a236c82c9df
George Dilthey <george.dilthey@gmail.com>
2021-08-23 22:06:18 -0400

    commit tree 950577e2b4876713987d27bb121780292feaa36b

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-'" "]+/g, '');
+  }
+
+  static titleize(string){
+    let final = []
+    let notAllowed = ["the", "a", "an", "but", "of", "and", "for", "at", "by", "from"]
+    string.split(" ").map(function(word){
+      if (!notAllowed.includes(word) ){
+        final.push(word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+      }
+      else { 
+        final.push(word)
+      }  
+    })
+    return this.capitalize(final.join(" "))
+  }
+
 }
